00d990440f8c208b9d3e060a458e1ed4279997d6
Guilherme-Mesquita06 <guilherme.mesquita@sptech.school>
2025-12-08 15:33:50 -0300

    Fix: corrigindo marge

--- Neoguard/Web-data-viz/src/controllers/historicoController.js
+++ Neoguard/Web-data-viz/src/controllers/historicoController.js
@@ -1,60 +1,42 @@
 var historicoModel = require('../models/historicoModel');
 
-
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var temp = req.body.tempServer
-    var alerta = req.body.alertaServer
-    var idSensor = req.body.idSensorServer
 
+    var temp = req.body.tempServer;
+    var alerta = req.body.alertaServer;
+    var idSensor = req.body.idSensorServer;
 
-    // Faça as validações dos valores
-    if (alerta == undefined) {
-        res.status(400).send("Seu periodoGestacao está undefined!");
-    } else if (idIncubadora == undefined) {
-        res.status(400).send("Seu idIncubadora está undefined!");
-    }else if (temp == undefined) {
-            res.status(400).send("Seu temperatura está undefined!");
-        } else {
+    if (temp == undefined) return res.status(400).send("Temperatura undefined");
+    if (alerta == undefined) return res.status(400).send("Alerta undefined");
+    if (idSensor == undefined) return res.status(400).send("ID Sensor undefined");
 
-            // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-            historicoModel.cadastrar(temp, alerta, idSensor)
-                .then(
-                    function (resultado) {
-                        res.json(resultado);
-                    }
-                ).catch(
-                    function (erro) {
-                        console.log(erro);
-                        console.log(
-                            "\nHouve um erro ao realizar o cadastro! Erro: ",
-                            erro.sqlMessage
-                        );
-                        res.status(500).json(erro.sqlMessage);
-                    }
-                );
-        }
-    }
-
-
-function contagem(req, res) {
-    const idHospital = req.params.idHospital;
-    const idSala = req.params.idSala;
-    const idIncubadora = req.params.idIncubadora;
-
-    historicoModel.contagem(idHospital, idSala, idIncubadora)
+    historicoModel.cadastrar(temp, alerta, idSensor)
         .then(resultado => res.json(resultado))
         .catch(erro => {
-            console.log("Erro ao buscar contagem:", erro.sqlMessage);
+            console.log("Erro cadastro:", erro.sqlMessage);
             res.status(500).json(erro.sqlMessage);
         });
 }
 
 
+function contagem(req, res) {
 
+    const idHospital = req.params.idHospital;
+    const idSala = req.params.idSala;
+    const idIncubadora = req.params.idIncubadora;
 
+    if (!idHospital || !idSala || !idIncubadora)
+        return res.status(400).send("Parâmetros inválidos");
 
-    module.exports = {
-        cadastrar,
-        contagem
-    };
+    historicoModel.contagem(idHospital, idSala, idIncubadora)
+        .then(resultado => res.json(resultado))
+        .catch(erro => {
+            console.log("Erro contagem:", erro.sqlMessage);
+            res.status(500).json(erro.sqlMessage);
+        });
+}
+
+module.exports = {
+    cadastrar,
+    contagem
+};
--- Neoguard/Web-data-viz/src/models/historicoModel.js
+++ Neoguard/Web-data-viz/src/models/historicoModel.js
@@ -1,35 +1,35 @@
 var database = require("../database/config");
 
-function cadastrar(temp,  alerta, fkSensor) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarAlertas():", temp, alerta, fkSensor);
-
+function cadastrar(temp, alerta, fkSensor) {
 
     var instrucaoSql = `
-     INSERT INTO historico (Temperatura, dataHora, Alerta, fkSensor)
-        VALUES ('${temp}', NOW(), '${alerta}', ${fkSensor});    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        INSERT INTO historico (Temperatura, dataHora, Alerta, fkSensor)
+        VALUES ('${temp}', NOW(), '${alerta}', ${fkSensor});
+    `;
+
+    console.log("Executando SQL:", instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function contagem( sala) {
-        console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarAlertas():", sala);
+function contagem(idHospital, idSala, idIncubadora) {
 
     const instrucaoSql = `
-      SELECT 
+        SELECT 
             SUM(alerta = 'Ideal') AS ideal,
             SUM(alerta = 'Critico') AS critico,
-            SUM(alerta = 'Atenção') AS atencao
+            SUM(alerta = 'Atencao') AS atencao
         FROM historico h
         JOIN sensor s ON s.id = h.fkSensor
         JOIN incubadora i ON i.fkSensor = s.id
         JOIN salaNeoNatal sn ON sn.id = i.fkSalaNeoNatal
         WHERE sn.id = ${idSala}
-        AND i.id = ${idIncubadora};
+        AND i.id = ${idIncubadora}
+        AND s.fkHospital = ${idHospital};
     `;
+
+    console.log("Executando SQL:", instrucaoSql);
     return database.executar(instrucaoSql);
 }
-
-
 
 module.exports = {
     cadastrar,
